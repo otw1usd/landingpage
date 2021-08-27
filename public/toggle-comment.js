96fd0a54feb6ad9b417ef62aeb01037ea44a078a
Kevin Putra <kvntryd@gmail.com>
2021-08-27 15:48:29 +0700

    toggle field photo aman + error terkait css udah aman

--- public/toggle-comment.js
+++
@@ -1,7 +0,0 @@
-//jshint esversion:6
-
-document.querySelectorAll(".toggle-comment-button").forEach(button => {
-  button.addEventListener("click", () => {
-    button.parentElement.nextElementSibling.classList.toggle("reply-comment-active");
-  });
-});
